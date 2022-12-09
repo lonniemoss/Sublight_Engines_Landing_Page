a791e93d7eef369432f8b6a8819e02a2dc9497a1
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2022-12-09 15:10:24 -0500

    navbar and form updated

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navOptions.classList.remove("active");
 }))
+
+function validateForm() {
+    let x = document.forms["myForm"]["name"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+}
+
+
